d75ce407b6be7efa59cd286f702ea11da1bc3325
Rob <rjboyce@teksavvy.com>
2020-04-13 19:28:19 -0400

    Refactored POM to fully use BOM

--- target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-04-13T17:29:06-0400",
+    date = "2020-04-13T19:23:05-0400",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.3 (Oracle Corporation)"
 )
 @Component
